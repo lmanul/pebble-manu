e3fea0a0362ab16a8c64933307b3b851dbadee38
Manu Cornet <m@ma.nu>
2019-01-10 13:48:07 +0000

    Cleanup

--- src/ks-clock-face.c
+++ src/ks-clock-face.c
@@ -8,17 +8,15 @@ static GFont s_font;
 const int LINE_HEIGHT = 40;
 
 static void update_time() {
-    // Get a tm structure
-    time_t temp = time(NULL);
-    struct tm *tick_time = localtime(&temp);
+    time_t current_time = time(NULL);
+    struct tm *utc_tm = gmtime(&current_time);
 
     // Write the current hours and minutes into a buffer
     static char s_hours_buffer[8];
-    strftime(s_hours_buffer, sizeof(s_hours_buffer), "%H:%M", tick_time);
+    strftime(s_hours_buffer, sizeof(s_hours_buffer), "%H:%M", utc_tm);
 
     // Display this time on the TextLayer
     text_layer_set_text(s_hours_layer, s_hours_buffer);
-
 }
 
 static void tick_minute_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -29,9 +27,7 @@ static void main_window_load(Window *window) {
     // Get information about the Window
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
-
     s_font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
-    //s_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_OPEN_SANS_70));
 
     // Create the TextLayer with specific bounds
     s_hours_layer = text_layer_create(GRect(0, 0, bounds.size.w, LINE_HEIGHT));
@@ -44,18 +40,6 @@ static void main_window_load(Window *window) {
 
     // Add it as a child layer to the Window's root layer
     layer_add_child(window_layer, text_layer_get_layer(s_hours_layer));
-
-    // Create the TextLayer with specific bounds
-    //s_minutes_layer = text_layer_create(GRect(0, 84, bounds.size.w, 84));
-
-    // Improve the layout to be more like a watchface
-    //text_layer_set_background_color(s_minutes_layer, GColorBlack);
-    //text_layer_set_text_color(s_minutes_layer, GColorWhite);
-    //text_layer_set_font(s_minutes_layer, s_font);
-    //text_layer_set_text_alignment(s_minutes_layer, GTextAlignmentCenter);
-
-    // Add it as a child layer to the Window's root layer
-    //layer_add_child(window_layer, text_layer_get_layer(s_minutes_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -66,8 +50,6 @@ static void main_window_unload(Window *window) {
     // Unload custom font
     fonts_unload_custom_font(s_font);
 }
-
-// ----
 
 static void init() {
     APP_LOG(APP_LOG_LEVEL_INFO, "init");
@@ -93,8 +75,6 @@ static void init() {
 
 static void deinit() {
     APP_LOG(APP_LOG_LEVEL_INFO, "deinit");
-
-    // Destroy Window
     window_destroy(s_main_window);
 }
 
