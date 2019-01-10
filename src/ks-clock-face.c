36975b5f3ad55a79ce3fcd0a5b1a69854cf63aa7
Manu Cornet <m@ma.nu>
2019-01-10 15:03:36 +0000

    More layout. Need arrays.

--- src/ks-clock-face.c
+++ src/ks-clock-face.c
@@ -3,14 +3,18 @@
 #define TZ_COUNT 1
 
 static Window *s_main_window;
-static TextLayer *s_name_layer;
-static TextLayer *s_time_layer;
+static TextLayer *s_name_layer1;
+static TextLayer *s_time_layer1;
+static TextLayer *s_name_layer2;
+static TextLayer *s_time_layer2;
 static GFont s_font;
 
 const int LINE_HEIGHT = 40;
-const int TZ_VALUE_X_POS = 50;
+const int X_PADDING = 10;
+const int Y_PADDING = 5;
+const int TZ_VALUE_X_POS = 100;
 
-const char* TZ_NAMES[] = {"UTC1", "UTC2", "UTC3", "UTC4", "UTC5"};
+const char* TZ_NAMES[] = {"UTC-1", "UTC-2", "UTC-3", "UTC-4", "UTC-5"};
 
 static void update_time() {
     time_t current_time = time(NULL);
@@ -20,8 +24,10 @@ static void update_time() {
     static char s_hours_buffer[8];
     strftime(s_hours_buffer, sizeof(s_hours_buffer), "%H:%M", utc_tm);
 
-    text_layer_set_text(s_time_layer, s_hours_buffer);
-    text_layer_set_text(s_name_layer, TZ_NAMES[0]);
+    text_layer_set_text(s_name_layer1, TZ_NAMES[0]);
+    text_layer_set_text(s_time_layer1, s_hours_buffer);
+    text_layer_set_text(s_name_layer2, TZ_NAMES[1]);
+    text_layer_set_text(s_time_layer2, s_hours_buffer);
 }
 
 static void tick_minute_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -34,28 +40,45 @@ static void main_window_load(Window *window) {
     GRect bounds = layer_get_bounds(window_layer);
     s_font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
 
-    s_name_layer = text_layer_create(GRect(0, 0, TZ_VALUE_X_POS, LINE_HEIGHT));
-    s_time_layer = text_layer_create(GRect(TZ_VALUE_X_POS, 0, bounds.size.w - TZ_VALUE_X_POS, LINE_HEIGHT));
+    s_name_layer1 = text_layer_create(GRect(0, 0, TZ_VALUE_X_POS - X_PADDING, LINE_HEIGHT));
+    s_time_layer1 = text_layer_create(GRect(TZ_VALUE_X_POS, 0, bounds.size.w - TZ_VALUE_X_POS, LINE_HEIGHT));
 
-    text_layer_set_background_color(s_name_layer, GColorWhite);
-    text_layer_set_text_color(s_name_layer, GColorBlack);
-    text_layer_set_font(s_name_layer, s_font);
-    text_layer_set_text_alignment(s_name_layer, GTextAlignmentRight);
+    s_name_layer2 = text_layer_create(GRect(
+        0, LINE_HEIGHT + Y_PADDING, TZ_VALUE_X_POS - X_PADDING, LINE_HEIGHT));
+    s_time_layer2 = text_layer_create(GRect(
+        TZ_VALUE_X_POS, LINE_HEIGHT + Y_PADDING, bounds.size.w - TZ_VALUE_X_POS, LINE_HEIGHT));
 
-    text_layer_set_background_color(s_time_layer, GColorWhite);
-    text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_font(s_time_layer, s_font);
-    text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+    text_layer_set_background_color(s_name_layer1, GColorWhite);
+    text_layer_set_text_color(s_name_layer1, GColorBlack);
+    text_layer_set_font(s_name_layer1, s_font);
+    text_layer_set_text_alignment(s_name_layer1, GTextAlignmentRight);
 
-    // Add it as a child layer to the Window's root layer
-    layer_add_child(window_layer, text_layer_get_layer(s_name_layer));
-    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+    text_layer_set_background_color(s_time_layer1, GColorWhite);
+    text_layer_set_text_color(s_time_layer1, GColorBlack);
+    text_layer_set_font(s_time_layer1, s_font);
+    text_layer_set_text_alignment(s_time_layer1, GTextAlignmentLeft);
+
+    text_layer_set_background_color(s_name_layer2, GColorWhite);
+    text_layer_set_text_color(s_name_layer2, GColorBlack);
+    text_layer_set_font(s_name_layer2, s_font);
+    text_layer_set_text_alignment(s_name_layer2, GTextAlignmentRight);
+
+    text_layer_set_background_color(s_time_layer2, GColorWhite);
+    text_layer_set_text_color(s_time_layer2, GColorBlack);
+    text_layer_set_font(s_time_layer2, s_font);
+    text_layer_set_text_alignment(s_time_layer2, GTextAlignmentLeft);
+
+    layer_add_child(window_layer, text_layer_get_layer(s_name_layer1));
+    layer_add_child(window_layer, text_layer_get_layer(s_time_layer1));
+    layer_add_child(window_layer, text_layer_get_layer(s_name_layer2));
+    layer_add_child(window_layer, text_layer_get_layer(s_time_layer2));
 }
 
 static void main_window_unload(Window *window) {
-    // Destroy TextLayer
-    text_layer_destroy(s_name_layer);
-    text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_name_layer1);
+    text_layer_destroy(s_time_layer1);
+    text_layer_destroy(s_name_layer2);
+    text_layer_destroy(s_time_layer2);
 
     // Unload custom font
     fonts_unload_custom_font(s_font);
