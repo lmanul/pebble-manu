e3dbdb13e770a46565057327bc48e778320b7908
Manu Cornet <m@ma.nu>
2019-01-10 14:53:06 +0000

    Starting a tiny bit of layout

--- src/ks-clock-face.c
+++ src/ks-clock-face.c
@@ -1,11 +1,15 @@
 #include <pebble.h>
 
+#define TZ_COUNT 1
+
 static Window *s_main_window;
-static TextLayer *s_hours_layer;
-static TextLayer *s_minutes_layer;
+static TextLayer *s_time_layer;
 static GFont s_font;
 
 const int LINE_HEIGHT = 40;
+const int TZ_VALUE_X_POS = 50;
+
+const char* TZ_NAMES[] = {"UTC1", "UTC2", "UTC3", "UTC4", "UTC5"};
 
 static void update_time() {
     time_t current_time = time(NULL);
@@ -16,7 +20,7 @@ static void update_time() {
     strftime(s_hours_buffer, sizeof(s_hours_buffer), "%H:%M", utc_tm);
 
     // Display this time on the TextLayer
-    text_layer_set_text(s_hours_layer, s_hours_buffer);
+    text_layer_set_text(s_time_layer, s_hours_buffer);
 }
 
 static void tick_minute_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -30,22 +34,21 @@ static void main_window_load(Window *window) {
     s_font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
 
     // Create the TextLayer with specific bounds
-    s_hours_layer = text_layer_create(GRect(0, 0, bounds.size.w, LINE_HEIGHT));
+    s_time_layer = text_layer_create(GRect(TZ_VALUE_X_POS, 0, bounds.size.w - TZ_VALUE_X_POS, LINE_HEIGHT));
 
     // Improve the layout to be more like a watchface
-    text_layer_set_background_color(s_hours_layer, GColorWhite);
-    text_layer_set_text_color(s_hours_layer, GColorBlack);
-    text_layer_set_font(s_hours_layer, s_font);
-    text_layer_set_text_alignment(s_hours_layer, GTextAlignmentLeft);
+    text_layer_set_background_color(s_time_layer, GColorWhite);
+    text_layer_set_text_color(s_time_layer, GColorBlack);
+    text_layer_set_font(s_time_layer, s_font);
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 
     // Add it as a child layer to the Window's root layer
-    layer_add_child(window_layer, text_layer_get_layer(s_hours_layer));
+    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
-    text_layer_destroy(s_hours_layer);
-    text_layer_destroy(s_minutes_layer);
+    text_layer_destroy(s_time_layer);
 
     // Unload custom font
     fonts_unload_custom_font(s_font);
